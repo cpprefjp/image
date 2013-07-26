7c31f81b500e5500837e9664011f5af2ada297a0
Akira Takahashi <faithandbrave@gmail.com>
2013-07-26 17:24:53 +0900

    コメントが古かったので修正

--- reference/random/random_stats.R
+++ reference/random/random_stats.R
@@ -6,11 +6,9 @@ png("random.png")
 # TSV形式になっている乱数の出力データを読み込む
 x <- read.table("random.tsv")
 
-# 1列目だけを抜き出す
+# 1列ずつ抜き出す
 value1 = x$V1
 value2 = x$V2
-
-# 以下、データの特性に合わせて、ヒストグラムかプロットかを選択してください。
 
 # 折れ線グラフを2つ混ぜて出力
 plot(density(value1), xlim=c(-1,4), ylim=c(0.0,0.8), type="l")
