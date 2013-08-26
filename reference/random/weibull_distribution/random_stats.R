33edea7126b40a87631cbf302ba9b96db6c187e3
Akira Takahashi <faithandbrave@gmail.com>
2013-08-26 23:09:34 +0900

    weibull_distributionの図を追加

---
+++ reference/random/weibull_distribution/random_stats.R
@@ -0,0 +1,16 @@
+# 乱数の分布クラスを使用した結果の出力を、図に変換するRスクリプト
+
+# png形式で出力する
+png("weibull_distribution.png")
+
+# TSV形式になっている乱数の出力データを読み込む
+x <- read.table("weibull_distribution.tsv")
+
+# 1列ずつ抜き出す
+value1 = x$V1
+value2 = x$V2
+
+# プロットとして出力
+plot(density(value1), xlim=c(-1,10), ylim=c(0.0,0.8), lty="longdash", col=rgb(0,1,0))
+par(new=T)
+plot(density(value2), xlim=c(-1,10), ylim=c(0.0,0.8), col=rgb(0,0,1))
