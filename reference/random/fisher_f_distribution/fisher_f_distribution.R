edd2b038745814dd9cad568ded91e2cb5f81486e
Mitsuru Kariya <kariya_mitsuru@hotmail.com>
2015-09-17 03:18:42 +0900

    fisher_f_distribution: tsv ファイルを再度生成＆グラフを更新＆Rスクリプト追加

---
+++ reference/random/fisher_f_distribution/fisher_f_distribution.R
@@ -0,0 +1,19 @@
+# 乱数の分布クラスを使用した結果の出力を、図に変換するRスクリプト
+
+# png形式で出力する
+png("fisher_f_distribution.png")
+
+# TSV形式になっている乱数の出力データを読み込む
+x <- read.table("fisher_f_distribution.tsv")
+
+# 1列目だけを抜き出す
+value = x$V1
+
+# ｸﾞﾗﾌを縦に2つ
+par(mfrow=c(2, 1))
+
+# 1つ目：データをプロットとして出力
+plot(value)
+
+# 2つ目：密度をプロットとして出力
+plot(density(value))
