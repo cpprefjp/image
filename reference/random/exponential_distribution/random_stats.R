6c721858c1983ab8c5ea3b168ff98c395fa4b6c2
Akira Takahashi <faithandbrave@gmail.com>
2024-02-02 03:18:25 +0900

    exponential_distributionのヒストグラム図を追加 #5

---
+++ reference/random/exponential_distribution/random_stats.R
@@ -0,0 +1,15 @@
+# 乱数の分布クラスを使用した結果の出力を、図に変換するRスクリプト
+
+# png形式で出力する
+png("exponential_distribution-hist.png")
+
+# TSV形式になっている乱数の出力データを読み込む
+x <- read.table("exponential_distribution.tsv")
+
+# 1列目だけを抜き出す
+value = x$V1
+
+# 以下、データの特性に合わせて、ヒストグラムかプロットかを選択してください。
+
+# ヒストグラムとして出力
+hist(value)
