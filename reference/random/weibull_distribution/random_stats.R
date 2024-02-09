36d3df3317994adb2c437208e084898072e599dc
Akira Takahashi <faithandbrave@gmail.com>
2024-02-09 15:21:56 +0900

    weibull_distribution : 負の値が出力されるように見えるので透明なヒストグラムにした #5

--- reference/random/weibull_distribution/random_stats.R
+++ reference/random/weibull_distribution/random_stats.R
@@ -7,10 +7,13 @@ png("weibull_distribution.png")
 x <- read.table("weibull_distribution.tsv")
 
 # 1列ずつ抜き出す
-value1 = x$V1
+values = x$V1
 value2 = x$V2
 
 # プロットとして出力
-plot(density(value1), xlim=c(-1,10), ylim=c(0.0,0.8), lty="longdash", col=rgb(0,1,0))
-par(new=T)
-plot(density(value2), xlim=c(-1,10), ylim=c(0.0,0.8), col=rgb(0,0,1))
+#plot(density(value1), xlim=c(-1,10), ylim=c(0.0,0.8), lty="longdash", col=rgb(0,1,0))
+#par(new=T)
+#plot(density(value2), xlim=c(-1,10), ylim=c(0.0,0.8), col=rgb(0,0,1))
+
+hist(values, col=rgb(0,1,0,0.2), main="")
+hist(value2, col=rgb(0,0,1,0.2), add=T)
