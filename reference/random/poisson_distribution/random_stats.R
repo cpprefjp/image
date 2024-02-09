9bd5e8e39d54b83b7a0abd935fa01000aded24b0
Akira Takahashi <faithandbrave@gmail.com>
2024-02-09 13:42:42 +0900

    poisson_distribution : 離散分布であるためplotからhistgramに変更 #5

---
+++ reference/random/poisson_distribution/random_stats.R
@@ -0,0 +1,15 @@
+# 乱数の分布クラスを使用した結果の出力を、図に変換するRスクリプト
+
+# png形式で出力する
+png("poisson_distribution.png")
+
+# TSV形式になっている乱数の出力データを読み込む
+x <- read.table("poisson_distribution.tsv")
+
+# 1列目だけを抜き出す
+value = x$V1
+
+# 以下、データの特性に合わせて、ヒストグラムかプロットかを選択してください。
+
+# ヒストグラムとして出力
+hist(value)
